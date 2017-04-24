8b5b561f91549f56a4e448005191364c921e3ac7
Kevin Isom <kevin.isom@gmail.com>
2017-04-24 22:51:44 +1200

    fix the text

--- bin/bolt-on.js
+++ bin/bolt-on.js
@@ -3,28 +3,23 @@
 const program = require('commander');
 const package = require('../package.json');
 const templates = require('../packages');
+const ora = require('ora');
+
+
 
 program
   .version(package.version)
-  .command('on <req> [optional]')
-  .description('add description')
-  .option('-o, --option', `we can still have add'l options`)
+  .command('on <req>')
+  .description('Bolt on different JavaScript enhancements')
   .action(function(req, optional){
-    console.log('.action() allows us to implement the command');
-    console.log('User passed %s', req);
-    console.log('cwd', process.cwd());
-    console.log('__dirname', __dirname);
+    const spinner = ora(` Bolting on ${req}`).start();
+
     if (templates[req]) {
       templates[req](process.cwd()).then(() => {
-        console.log('what was promised');
+        spinner.succeed(' Bolt on complate');
       });
     } else {
-      console.log('nothing there');
-    }
-    if (optional) {
-      optional.forEach(function(opt){
-        console.log("User passed optional arguments: %s", opt);
-      });
+      spinner.fail('There was nothing to bolt on');
     }
   });
 program.parse(process.argv);
