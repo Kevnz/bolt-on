b7e73e2cf62fd0ff8473afb5c5aeb660bb6ddfb9
Kevin Isom <kevin.isom@gmail.com>
2017-08-30 12:18:07 +1200

    spinner

--- bin/bolt-on.js
+++ bin/bolt-on.js
@@ -5,14 +5,12 @@ const package = require('../package.json');
 const templates = require('../packages');
 const ora = require('ora');
 
-
-
 program
   .version(package.version)
   .command('on <req>')
   .description('Bolt on different JavaScript enhancements')
   .action(function(req, optional){
-    const spinner = ora(` Bolting on ${req}`).start();
+    const spinner = ora({text: ` Bolting on ${req}`, spinner: 'shark'}).start();
 
     if (templates[req]) {
       templates[req](process.cwd()).then(() => {
